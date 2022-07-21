a686047c97d2feec8d32b5175e6b659964e7177e
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-20 18:00:11 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target["new-task-description"].value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  console.log(li);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+
